f0910b75a3eaf0b6f694291c87d0858911b39552
kamalmog <168238963+kamalmog@users.noreply.github.com>
2025-07-05 14:53:46 -0400

    ee

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -1,25 +1,8 @@
+
 package main
 
-import (
-	"fmt"
-)
-
-type Request struct {
-	Name string `json:"name"`
-}
-
-type Response struct {
-	StatusCode int               `json:"statusCode,omitempty"`
-	Headers    map[string]string `json:"headers,omitempty"`
-	Body       string            `json:"body,omitempty"`
-}
-
-func Main(in Request) (*Response, error) {
-	if in.Name == "" {
-		in.Name = "stranger"
-	}
-
-	return &Response{
-		Body: fmt.Sprintf("Hello %s!", in.Name),
-	}, nil
+func Main(args map[string]interface{}) map[string]interface{} {
+msg := make(map[string]interface{})
+msg["success"] = "YEY WE MADE IT"
+	return msg
 }
