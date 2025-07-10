20d06cc8955364ac3fa08095ee95a35e851b1873
kamalmog <168238963+kamalmog@users.noreply.github.com>
2025-07-09 21:45:30 -0400

    Update Main.go

--- hellofunction/Main.go
+++ hellofunction/Main.go
@@ -12,11 +12,17 @@ type Response struct {
 	Body string `json:"body"`
 }
 
-func Main(ctx context.Context, event Event) Response {
+/*func Main(ctx context.Context, event Event) Response {
 	if event.Name == "" {
 		event.Name = "stranger"
 	}
 	version := ctx.Value("function_version").(string)
+	return Response {
+		Body: "Hello " + event.Name + "! This is function version " + version,
+	}
+}*/
+
+func Main(params map[string]interface{}) map[string]interface{}{
 	return Response {
 		Body: "Hello " + event.Name + "! This is function version " + version,
 	}
